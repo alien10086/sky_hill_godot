83bb9f80df2bb234d188b575a881145cae7a30cd
LGZ <oppo0310@qq.com>
2025-12-10 21:53:00 +0800

    feat(level_x): 添加随机房间模板功能
    实现左右房间的随机模板替换功能，通过定义ROOM_TEMPLATES常量列表和添加_replace_left_room_templates、_replace_right_room_templates方法，在_ready时随机选择模板并替换左右房间

--- scenes/world/level_x.gd
+++ scenes/world/level_x.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+# 房间模板路径
+const ROOM_TEMPLATES = [
+	"res://scenes/world/normal_room_template/base_room_1.tscn",
+	"res://scenes/world/normal_room_template/base_room_2.tscn",
+	"res://scenes/world/normal_room_template/base_room_3.tscn",
+	"res://scenes/world/normal_room_template/base_room_4.tscn"
+]
 
 @onready var right_room: NormalRoomUI = $RightRoom
 @onready var left_room: NormalRoomUI = $LeftRoom
@@ -11,6 +18,43 @@ var wall_paper_manager: WallpaperManager
 
 func _ready() -> void:
 	wall_paper_manager = WallpaperManager.get_instance()
+	
+	# 随机选择房间模板并替换左右房间
+	_replace_left_room_templates()
+	_replace_right_room_templates()
+
+func _replace_left_room_templates():
+	# 随机选择一个房间模板
+	var random_template_index = randi() % ROOM_TEMPLATES.size()
+	var template_path = ROOM_TEMPLATES[random_template_index]
+	var room_scene =  load(template_path)
+	
+	# 替换左房间
+	var left_room_parent = left_room.get_parent()
+	var left_room_position = left_room.position
+	left_room.queue_free()
+	var new_left_room = room_scene.instantiate()
+	new_left_room.name = "LeftRoom"
+	new_left_room.position = left_room_position
+	left_room_parent.add_child(new_left_room)
+	left_room = new_left_room
+	
+
+func _replace_right_room_templates():
+	# 随机选择一个房间模板
+	var random_template_index = randi() % ROOM_TEMPLATES.size()
+	var template_path = ROOM_TEMPLATES[random_template_index]
+	var room_scene = load(template_path)
+	
+	# 替换右房间
+	var right_room_parent = right_room.get_parent()
+	var right_room_position = right_room.position
+	right_room.queue_free()
+	var new_right_room = room_scene.instantiate()
+	new_right_room.name = "RightRoom"
+	new_right_room.position = right_room_position
+	right_room_parent.add_child(new_right_room)
+	right_room = new_right_room
 
 func set_level(floor:int):
 	center_room.level_number = floor
