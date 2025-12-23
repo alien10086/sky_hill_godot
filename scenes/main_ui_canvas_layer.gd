4ca2090854d0221f64a7b2f2ee13ffc5bda17732
LGZ <oppo0310@qq.com>
2025-12-23 21:22:02 +0800

    refactor(ui): 优化物品图标位置计算方式
    使用预定义的Marker2D节点替代手动计算位置
    将物品图标添加到正确的CanvasLayer层级

---
+++ scenes/main_ui_canvas_layer.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+
+class_name MainCanvasLayerUI
+
+@onready var center_marker_2d: Marker2D = $CenterMarker2D
+@onready var bag_marker_2d_2: Marker2D = $BagMarker2D2
--- scenes/player_info/eye_item.gd
+++ scenes/player_info/eye_item.gd
@@ -4,7 +4,7 @@ const UI_EYE = preload("uid://l5pvqrq3vshh")
 const UI_EYE_1 = preload("uid://c2f3bp4ife2g0")
 
 @onready var texture_button: TextureButton = $"."
-@export var ui_canvas_layer:CanvasLayer
+@export var ui_canvas_layer:MainCanvasLayerUI
 
 var is_open: bool = false
 var inventory_manage: InventoryManage
@@ -22,18 +22,19 @@ func _ready() -> void:
 func get_layer_center() -> Vector2:
 	# get_viewport() 会返回该节点所属的视口
 	# get_visible_rect() 返回视口在屏幕上的可见矩形范围
-	var center = ui_canvas_layer.get_viewport().get_visible_rect().size / 2
-	center.y = center.y - 300
+	return ui_canvas_layer.center_marker_2d.global_position
+	#var center = ui_canvas_layer.get_viewport().get_visible_rect().size / 2
+	#center.y = center.y - 300
 	
-	return center
+	#return center
 
 func get_layer_left_bottom_center() -> Vector2:
 	# get_viewport() 会返回该节点所属的视口
 	# get_visible_rect() 返回视口在屏幕上的可见矩形范围
-	var center = ui_canvas_layer.get_viewport().get_visible_rect().size / 2
-	center.x = center.x - 1000
-	center.y = center.y  + 200
-	return center
+	#var center = ui_canvas_layer.get_viewport().get_visible_rect().size / 2
+	#center.x = center.x - 1000
+	#center.y = center.y  + 200
+	return ui_canvas_layer.bag_marker_2d_2.global_position
 
 
 func _on_pressed() -> void:
@@ -48,11 +49,11 @@ func _on_pressed() -> void:
 	# 创建物品图标并执行飞行动画
 	var item_icon = TextureRect.new()
 	item_icon.texture = random_item_data.load_icon() 
-	item_icon.position = get_global_position()  
+	item_icon.global_position = global_position  
 	item_icon.size = Vector2(64, 64)  # 设置合适的图标大小
 	item_icon.z_index = 100
 	# 添加到场景根节点
-	get_tree().get_root().add_child(item_icon)
+	ui_canvas_layer.add_child(item_icon)
 	#get_parent().get_parent().add_child(item_icon)
 	# 获取屏幕中间和左下角位置
 	#var screen_center = Vector2(ui_canvas_layer.custom_viewport.size.x / 2, ui_canvas_layer.custom_viewport.size.y / 2 - 300)
@@ -60,7 +61,7 @@ func _on_pressed() -> void:
 	#screen_bottom_left
 	# 创建Tween动画
 	var tween = create_tween()
-	
+	#
 	# 第一阶段：飞到屏幕中间
 	tween.tween_property(item_icon, "global_position", get_layer_center(), 1.5) \
 		.set_trans(Tween.TRANS_QUAD) \
