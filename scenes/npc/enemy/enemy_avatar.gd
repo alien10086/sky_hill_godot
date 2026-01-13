8e5061e25f7066a4927fb9c738c0515b088bc1f3
LGZ <oppo0310@qq.com>
2026-01-13 21:27:15 +0800

    feat(ui): 添加敌人头像界面及相关资源文件
    新增敌人头像UI场景，包含头像显示和血条组件
    添加四种敌人头像图片资源及对应的导入配置

---
+++ scenes/npc/enemy/enemy_avatar.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+const BIG_FAT_HEAD_UI = preload("uid://ual3ckersvco")
+const LONG_ARM_HEAD_UI = preload("uid://dts2ldlvrljo8")
+
+@onready var texture_rect_2: TextureRect = $Head/TextureRect2
+@onready var label: Label = $HeartHub/Label
