6a3812c2f951d267abf79c93122f2cb3f419ad2a
LGZ <oppo0310@qq.com>
2026-03-06 22:02:09 +0800

    feat(menu): 添加菜单场景背景音乐和环境音效
    在菜单场景就绪时播放主背景音乐和下雨环境音。
    使用 SFX 播放器播放下雨音，以支持与背景音乐同时播放。

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,6 +2,21 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var bgm_music = preload("res://assets/audio/bgm/menu_theme.wav")
+var bgm_rain = preload("res://assets/audio/bgm/rain.wav")
+
 func _ready() -> void:
 	if animation_player:
 		animation_player.play("light")
+	
+	_play_menu_audio()
+
+func _play_menu_audio():
+	# 播放主背景音乐
+	if bgm_music:
+		AudioManager.play_bgm(bgm_music)
+	
+	# 播放下雨环境音 (使用 SFX 播放器，因为它支持多轨道且我们需要它和 BGM 同时响)
+	if bgm_rain:
+		AudioManager.play_sfx(bgm_rain)
+		
